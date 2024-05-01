9d0b286ce37bb841f1545c55fa05f55260dbc39f
jobscale <jobscale@users.noreply.github.com>
2024-05-01 09:36:29 +0900

    docker run script added

---
+++ run-docker.sh
@@ -0,0 +1,5 @@
+{
+  docker run --privileged --name wetty --restart always -p 2998:3000 \
+  --ulimit nofile=128:256 --ulimit nproc=32:64 -d ghcr.io/jobscale/wetty:dind
+  docker logs --since 5m -f wetty
+}
